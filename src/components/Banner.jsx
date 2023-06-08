a6b6b2239d5baaf394a5e33f00dbf5dfe7a91f8d
Noizenne <karlamae2405@gmail.com>
2023-06-08 19:52:54 +0200

    Created banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import me from "../assets/img/me.jpg";
+
+function Banner() {
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="d-flex justify-content-center align-items-center">
+          <Col xs={7} md={5} xl={4} className="d-flex justify-content-center">
+            <img className="me" src={me} alt="Karla Mae Rabe" />
+          </Col>
+          <Col xs={12} md={6} xl={7}>
+            <div className="bannerItem">
+              <p>Hi, welcome to my portfolio</p>
+              <h2 className="tagline">My name is Karla Mae Rabe</h2>
+              <h3 className="tagline">And I'm a Frontend Developer student</h3>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default Banner;
